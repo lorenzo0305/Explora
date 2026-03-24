6a82e0851f528c13b80e437895ea5a95d8132ce3
Andrea <andreapassave@gmail.com>
2026-03-24 15:40:54 +0100

    Affichage de si ça stocke bien

--- webapp/static/scripts/Creation.js
+++ webapp/static/scripts/Creation.js
@@ -58,7 +58,8 @@ document.getElementById('criteriaForm').addEventListener('submit', async functio
         // Pour garder les données même après la redirectoin
         sessionStorage.setItem('algorithmResponse', JSON.stringify(data));
         sessionStorage.setItem('criteriaFormPayload', JSON.stringify(formData));
-        window.location.href = '/voyage.html';
+        console.log("Données stockées en sessionStorage pour la page suivante.");
+        window.location.href = '/Voyage.html';
 
     } catch (error) {
         console.error("Erreur lors de la soumission :", error);
