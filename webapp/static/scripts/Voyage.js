1c99ff0c48232232afa991925c63e97144fe16b0
tristanbtt <tristan.boutet20@gmail.com>
2026-03-24 17:17:16 +0100

    COmmencement de modification de voyage.js et du main. Implémentation des fausses données dans le main ppur testet

--- webapp/static/scripts/Voyage.js
+++ webapp/static/scripts/Voyage.js
@@ -1,9 +1,16 @@
-addEventListener.document('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const data = sessionStorage.getItem('algorithmRes');
+    
     if (!data) {
         console.error("Aucune donnée trouvée dans le stockage session.");
         window.location.href = '/Creation.html';
-        return;
+        return; // Stoppe l'exécution du reste du code
     }
+    
     const dataVoyage = JSON.parse(data);
+    
+    // Pour vérifier que tout fonctionne bien :
+    console.log("Mes données prêtes à être affichées :", dataVoyage);
+
+    // C'est ici qu'on va générer le HTML !
 });
