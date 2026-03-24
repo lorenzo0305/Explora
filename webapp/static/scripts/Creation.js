ea8fac32e45c4e412c25592b8d1de71114b09721
tristanbtt <tristan.boutet20@gmail.com>
2026-03-24 15:34:27 +0100

    Modification de creation.js pour qu'on garde les données après redirection

--- webapp/static/scripts/Creation.js
+++ webapp/static/scripts/Creation.js
@@ -58,7 +58,7 @@ document.getElementById('criteriaForm').addEventListener('submit', async functio
         // Pour garder les données même après la redirectoin
         sessionStorage.setItem('algorithmResponse', JSON.stringify(data));
         sessionStorage.setItem('criteriaFormPayload', JSON.stringify(formData));
-        window.location.href = '/voyage';
+        window.location.href = '/voyage.html';
 
     } catch (error) {
         console.error("Erreur lors de la soumission :", error);
